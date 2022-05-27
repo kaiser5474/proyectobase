93dc1e3387346c6986b5b7f4438dd085b7b7e0f7
kaiser5474 <jarvismiranda728096@gmail.com>
2022-05-27 09:54:27 -0400

    App Letras

---
+++ src/components/AppLetras.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Formulario from './Formulario'
+
+const AppLetras = () => {
+  return (
+      <>
+        <header>Busqueda de letras de canciones</header>
+        <Formulario/>
+        <main>
+
+        </main>
+      </>    
+  )
+}
+
+export default AppLetras
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,44 @@
+import {useState} from 'react'
+
+const Formulario = () => {
+    const [busqueda, setBusqueda] = useState({
+        artista: '',
+        cancion: ''
+    })
+  return (
+    <form>
+        <legend>Busca por artista y canción</legend>
+        <div className='form-grid'>
+            <div>
+                <label>Artista</label>
+                <input 
+                    type='text'
+                    name='artista'
+                    placeholder='Busca por artista'
+                    value={busqueda.artista}
+                    onChange={(e) => setBusqueda({
+                        ...busqueda,
+                        [e.target.name] : e.target.value 
+                    })}
+                />
+            </div>
+            <div>
+                <label>Canción</label>
+                <input 
+                    type='text'
+                    name='cancion'
+                    placeholder='Busca por canción'
+                    value={busqueda.cancion}
+                    onChange={(e) => setBusqueda({
+                        ...busqueda,
+                        [e.target.name] : e.target.value 
+                    })}
+                />
+            </div>
+            <input type="submit"/>
+        </div>
+    </form>
+  )
+}
+
+export default Formulario
